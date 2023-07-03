ed58b91954f0e9a88ee77212fe8ae11e0d404cfa
mohamedfattouhy <78499945+mohamedfattouhy@users.noreply.github.com>
2023-07-03 17:21:50 +0200

    file containing the neural network model

---
+++ facedetection/custom_model/build_model.py
@@ -0,0 +1,49 @@
+# MANAGE ENVIRONMENT
+import tensorflow as tf
+from keras.models import Model
+from keras.layers import Input, Dense, GlobalMaxPooling2D
+from keras.applications import VGG16
+
+# Download VGG16 model
+vgg = VGG16(include_top=False)
+# print()
+# print(vgg.summary())
+
+
+# Build instance of Neural Network
+def build_model():
+
+    input_layer = Input(shape=(120, 120, 3))
+
+    vgg = VGG16(include_top=False)(input_layer)
+
+    # Classification Model
+    f1 = GlobalMaxPooling2D()(vgg)
+    class1 = Dense(2048, activation='relu')(f1)
+    class2 = Dense(1, activation='sigmoid')(class1)
+
+    # Bounding box model
+    f2 = GlobalMaxPooling2D()(vgg)
+    regress1 = Dense(2048, activation='relu')(f2)
+    regress2 = Dense(4, activation='sigmoid')(regress1)
+
+    facetracker = Model(inputs=input_layer, outputs=[class2, regress2])
+
+    return facetracker
+
+
+# Create Localization Loss and Classification Loss
+def localization_loss(y_true, yhat):
+
+    delta_coord = tf.reduce_sum(tf.square(y_true[:, :2] - yhat[:, :2]))
+
+    h_true = y_true[:, 3] - y_true[:, 1]
+    w_true = y_true[:, 2] - y_true[:, 0]
+
+    h_pred = yhat[:, 3] - yhat[:, 1]
+    w_pred = yhat[:, 2] - yhat[:, 0]
+
+    delta_size = tf.reduce_sum(tf.square(w_true - w_pred)
+                               + tf.square(h_true-h_pred))
+
+    return delta_coord + delta_size
