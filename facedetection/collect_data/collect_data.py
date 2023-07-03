8c55111652ac15d30c754af50d7df04feb660839
mohamedfattouhy <78499945+mohamedfattouhy@users.noreply.github.com>
2023-07-03 16:00:48 +0200

    add file to collect data

---
+++ facedetection/collect_data/collect_data.py
@@ -0,0 +1,26 @@
+# MANAGE ENVIRONMENT
+import os
+import time
+import uuid
+import cv2
+
+
+def collect_data(save_path, n_images):
+
+    cap = cv2.VideoCapture(0)
+
+    for imgnum in range(n_images):
+
+        print('Collecting image {}'.format(imgnum))
+        _, frame = cap.read()
+        img_name = os.path.join(save_path, f'{str(uuid.uuid1())}.jpg')
+
+        cv2.imwrite(img_name, frame)
+        cv2.imshow('frame', frame)
+        time.sleep(5)
+
+        if cv2.waitKey(1) & 0XFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
